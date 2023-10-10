0f0ceafe569959adc34fc8e767d73a3952d21f6a
home <taisiyamusically@gmail.com>
2023-10-10 20:48:24 +0500

    commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print ("hello world")
+name = input('Taisiia')
+age =input('15')
+print(name)
+print(age)
+
+"""example"""
+
+a = int(input(1))  # input first num
+b = int(input(2))  # input second num
+print(a + b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**2) #возведение во 2 степень
+print(a**0,5) #корень
+
+"""example 2"""
+
+a, b = map(int, input().split())
+print(a)
+print(b)
+
+""" ooooo"""
+
+
+
+
+
